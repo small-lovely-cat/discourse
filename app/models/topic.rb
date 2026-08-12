e521ee6b3d9842641384b1f43952f04c480f221f
small-lovely-cat <77799160+small-lovely-cat@users.noreply.github.com>
2026-08-12 20:04:18 +0800

    Revert "FIX: Avoid traversing Redis keys when clearing 404 topic cache (#42457)"
    This reverts commit 97718ffa049d29f67327c940c274df7dabf82fdf.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -445,11 +445,7 @@ class Topic < ActiveRecord::Base
   end
 
   def self.clear_page_not_found_topics_cache!
-    keys =
-      I18n.available_locales.map do |locale|
-        Discourse.cache.normalize_key("page_not_found_topics:#{locale}")
-      end
-    Discourse.cache.redis.del(*keys)
+    Discourse.cache.keys("page_not_found_topics:*").each { |key| Discourse.cache.redis.del(key) }
   end
 
   def clear_page_not_found_topics_cache
